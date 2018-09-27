6ccd1d4257e04ab56531c6d4d8b2b7df17d888b2
dmitriy kalinin <dkalinin@pivotal.io>
2018-09-27 15:12:33 -0700

    add e2e for tcp/udp dns resolution

---
+++ test/e2e/net_dns_test.go
@@ -0,0 +1,74 @@
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+)
+
+type ResolverDialer struct {
+	Name     string
+	Func func(ctx context.Context, network, address string) (net.Conn, error)
+}
+
+func TestNetDNSResolution(t *testing.T) {
+	env := BuildEnv(t)
+	logger := Logger{}
+	kwt := Kwt{t, env.Namespace, Logger{}}
+	kubectl := Kubectl{t, env.Namespace, Logger{}}
+	kwtNet := NewKwtNet(kwt, t, Logger{})
+
+	kwtNet.Start()
+	defer kwtNet.End()
+
+	guestbookAddrs := Guestbook{kwt, kubectl, t, logger}.Install()
+
+	tcpDialer := ResolverDialer{
+		Name: "TCP",
+		Func: func(ctx context.Context, network, address string) (net.Conn, error) {
+			addr, err := net.ResolveTCPAddr("tcp", address)
+			if err != nil {
+				return nil, err
+			}
+			return net.DialTCP("tcp", nil, addr)
+		},
+	}
+
+	udpDialer := ResolverDialer{
+		Name: "UDP",
+		Func: func(ctx context.Context, network, address string) (net.Conn, error) {
+			addr, err := net.ResolveUDPAddr("udp", address)
+			if err != nil {
+				return nil, err
+			}
+			return net.DialUDP("udp", nil, addr)
+		},
+	}
+
+	for _, dialer := range []ResolverDialer{tcpDialer, udpDialer} {
+		dialer := dialer
+
+		logger.Section(fmt.Sprintf("Test DNS resolution over %s", dialer.Name), func() {
+			dnsResolver := net.Resolver{ // Not going to mdns
+				PreferGo:     true,
+				StrictErrors: true,
+				Dial:         dialer.Func,
+			}
+
+			addrs, err := dnsResolver.LookupIPAddr(context.TODO(), guestbookAddrs.FrontendSvcDomain)
+			if err != nil {
+				t.Fatalf("Expected DNS resolution to succeed: %s", err)
+			}
+
+			if len(addrs) != 1 {
+				t.Fatalf("Expected DNS resolution to result in one address, but was '%#v'", addrs)
+			}
+
+			if addrs[0].String() != guestbookAddrs.FrontendSvcIP {
+				t.Fatalf("Expected DNS resolution to result in one specific address '%s', but was '%s'",
+					guestbookAddrs.FrontendSvcIP, addrs[0].String())
+			}
+		})
+	}
+}
